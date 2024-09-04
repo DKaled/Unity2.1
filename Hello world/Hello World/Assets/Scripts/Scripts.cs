5f59c8bf1d436dd1c30769b595031ca8a8b12394
Derek Kaled <derekyoam98@gmail.com>
2024-09-04 05:01:57 -0600

    first commit

---
+++ Hello world/Hello World/Assets/Scripts/Scripts.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BtnReanudar() {
+        Debug.Log("El juego se reanudará");
+    }
+
+    public void BtnPlay() {
+        Debug.Log("El juego comenzará");
+    }
+
+    public void BtnSalir() {
+        Debug.Log("El juego se terminará");
+    }
+
+    public void icon1() {
+        Debug.Log("Bienvenido a la tienda");
+    }
+
+    public void icon2() {
+        Debug.Log("Bienvenido al chat");
+    }
+
+    public void icon3() {
+        Debug.Log("Bienvenido a Discord");
+    }
+
+    public void icon4() {
+        Debug.Log("Bienvenido a steam");
+    }
+
+    public void link1() {
+        Application.OpenURL ("http://facebook.com");
+    }
+
+    public void link2() {
+        Application.OpenURL ("http://instagram.com");
+    }
+
+    public void link3() {
+        Application.OpenURL ("http://x.com");
+    }
+
+
+}
